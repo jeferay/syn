c03389dfd5d0f672c67f677467cacc63f8821880
Dean-lyc <1700012978@pku.edu.cn>
2021-08-02 01:45:31 +0800

    Add Triplet model and candidates generation algorithm

--- code/models.py
+++ code/models.py
@@ -40,6 +40,29 @@ class Biosyn_Model(nn.Module):
         return score
 
 
+class SimpleEmbedding(nn.Module):
+    def __init__(self):
+        super(SimpleEmbedding, self).__init__()
+        self.layer = nn.Sequential(nn.Conv1d(1, 1, kernel_size=200+1-128), nn.PReLU(), nn.MaxPool1d(3, stride=1, padding=1))
+
+    def forward(self, x):
+        x = self.layer(x)
+        return x
+
+
+class TripletNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(TripletNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2, x3):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        output3 = self.embedding_net(x3)
+        return output1, output2, output3
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
 
 
 
