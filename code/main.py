a5d5a14561a9392e07a1c82db103018b581ee1e0
wzr <844570862@qq.com>
2021-08-04 22:15:27 +0800

    update stage 2

--- code/main.py
+++ code/main.py
@@ -1,10 +1,12 @@
+from ast import parse
 import numpy as np
 import random
 import os
 import torch
 import logging
 import argparse
-from classifier import  Biosyn_Classifier, Graphsage_Classifier
+
+from classifier import  Biosyn_Classifier, Graphsage_Classifier,CrossEncoder_Classifier
 
 
 
@@ -39,39 +41,44 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--filename',type=str,default='../data/datasets/cl.obo')
     parser.add_argument('--use_text_preprocesser',action='store_true',default=False)
-    parser.add_argument('--is_unseen',action='store_true',default=False)
+    parser.add_argument('--is_unseen',action='store_true',default=True)
     parser.add_argument('--device',type=str,default='cuda:0')
-    parser.add_argument('--model_path',type=str,default='../exp/eco/checkpoint_9')
+    parser.add_argument('--stage_1_model_path',type=str,default='../exp/cl/checkpoint_9')
+    parser.add_argument('--stage_1_exp_path',type=str,default='../exp/cl/stage_1')
+    parser.add_argument('--stage_2_model_path',type=str,default='../biobert')
+    parser.add_argument('--stage_2_exp_path',type=str,default='../exp/cl/stage_2')
+
     parser.add_argument('--vocab_file',type=str,default='../biobert/vocab.txt')
     parser.add_argument('--initial_sparse_weight',type=float,default=1.)
     parser.add_argument('--bert_ratio',type=float,default=0.5)
-    parser.add_argument('--bert_lr',type=float,default=1e-5)
-    parser.add_argument('--bert_weight_decay',type=float,default=1e-3)
-    parser.add_argument('--graph_lr',type=float,default=0.01)
-    parser.add_argument('--graph_weight_decay',type=float,default=5e-5)
+    parser.add_argument('--stage_1_lr',type=float,default=1e-5)
+    parser.add_argument('--stage_1_weight_decay',type=float,default=1e-5)
+    parser.add_argument('--stage_2_lr',type=float,default=1e-5)
+    parser.add_argument('--stage_2_weight_decay',type=float,default=5e-6)
 
-    parser.add_argument('--epoch_num',type=int,default=10)
+    parser.add_argument('--epoch_num',type=int,default=50)
     parser.add_argument('--top_k',type=int,default=20)
-    parser.add_argument('--batch_size',type=int,default=16)
-    parser.add_argument('--exp_path',type=str,default='../exp/cl')
+    parser.add_argument('--batch_size',type=int,default=8)
     parser.add_argument('--score_mode',type=str,default='hybrid')
     parser.add_argument('--eval_k',type=int,default=20)
     parser.add_argument('--seed',type=int,default=0)
     parser.add_argument('--save_checkpoint_all',action='store_true',default=True)
 
     
+   
     args = parser.parse_args()
     args = args.__dict__
     
     torch.cuda.set_device(args['device'])
     args['device'] = torch.device(args['device'] if torch.cuda.is_available() else 'cpu')
 
-    logger = setup_logger(name=args['exp_path'][:],log_file=os.path.join(args['exp_path'],'log.log'))
+    logger = setup_logger(name=args['stage_2_exp_path'][:],log_file=os.path.join(args['stage_2_exp_path'],'log.log'))
     args['logger'] = logger
 
     setup_seed(args['seed'])
 
-    bio = Graphsage_Classifier(args)
-    bio.train()
-    bio.eval(bio.queries_train)
+    b = CrossEncoder_Classifier(args)
+    b.eval_stage_1(b.queries_valid,epoch=0)
+    b.train_stage_2()
+
 
