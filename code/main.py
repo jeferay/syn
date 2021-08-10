5b0353a70858de335618496dcd59d08a418d0aff
wzr <844570862@qq.com>
2021-08-10 10:50:11 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -41,9 +41,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--filename',type=str,default='../data/datasets/cl.obo')
     parser.add_argument('--use_text_preprocesser',action='store_true',default=False)
-    parser.add_argument('--is_unseen',action='store_true',default=False)
-    parser.add_argument('--stage_1_model_path',type=str,default='../exp/cl/checkpoint_9')
-    parser.add_argument('--stage_1_exp_path',type=str,default='../exp/cl/stage_1')
+    parser.add_argument('--is_unseen',action='store_true',default=True)
+    parser.add_argument('--stage_1_model_path',type=str,default='../biobert')
+    parser.add_argument('--stage_1_exp_path',type=str,default='../exp/cl/stage_1/unseen')
     parser.add_argument('--stage_2_model_path',type=str,default='../exp/cl/checkpoint_9')
     parser.add_argument('--stage_2_exp_path',type=str,default='../exp/cl/stage_2')
 
@@ -51,17 +51,17 @@ if __name__ == '__main__':
     parser.add_argument('--initial_sparse_weight',type=float,default=1.)
     parser.add_argument('--bert_ratio',type=float,default=0.5)
     parser.add_argument('--stage_1_lr',type=float,default=1e-5)
-    parser.add_argument('--stage_1_weight_decay',type=float,default=1e-5)
+    parser.add_argument('--stage_1_weight_decay',type=float,default=0)
     parser.add_argument('--stage_2_lr',type=float,default=1e-2)
     parser.add_argument('--stage_2_weight_decay',type=float,default=0)
 
     parser.add_argument('--epoch_num',type=int,default=50)
-    parser.add_argument('--top_k',type=int,default=3)
-    parser.add_argument('--eval_k',type=int,default=3)
-    parser.add_argument('--batch_size',type=int,default=64)
+    parser.add_argument('--top_k',type=int,default=10)
+    parser.add_argument('--eval_k',type=int,default=10)
+    parser.add_argument('--batch_size',type=int,default=16)
     parser.add_argument('--score_mode',type=str,default='hybrid')
     parser.add_argument('--seed',type=int,default=0)
-    parser.add_argument('--save_checkpoint_all',action='store_true',default=True)
+    parser.add_argument('--save_checkpoint_all',action='store_true',default=False)
 
     
    
@@ -70,10 +70,16 @@ if __name__ == '__main__':
 
     logger = setup_logger(name=args['stage_2_exp_path'][:],log_file=os.path.join(args['stage_2_exp_path'],'log.log'))
     args['logger'] = logger
+    print(args)
 
     setup_seed(args['seed'])
-
-    b=Graph_Classifier(args)
     
-    b.train_stage_2()
+    #b= Graph_Classifier(args)
+    
+    b=CrossEncoder_Classifier(args)
+    #b.save_model_satge_1(os.path.join(args['stage_1_exp_path'],'checkpoint'))
+    #b.save_model_stage_2(checkpoint_dir=os.path.join(args['stage_2_exp_path'],'checkpoint'))
+
+    b.eval_stage_1(b.queries_valid,epoch=0)
+    b.train_stage_1()
 
